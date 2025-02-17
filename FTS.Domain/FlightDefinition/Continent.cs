91d439442de946fba0f749085228a85ef57333a6
Kumodoushin <Kumodoushin@users.noreply.github.com>
2025-02-17 07:25:36 +0100

    jsonconverter for Continent - lack of infrastructure project

--- FTS.Domain/FlightDefinition/Continent.cs
+++ FTS.Domain/FlightDefinition/Continent.cs
@@ -1,8 +1,9 @@
 ﻿using Ardalis.SmartEnum;
+using Newtonsoft.Json;
 
 namespace FTS.Domain;
 
-public sealed class Continent:SmartEnum<Continent,string>
+public sealed class Continent : SmartEnum<Continent,string>
 {
 	private Continent(string name, string value) : base(name, value)
 	{
@@ -14,4 +15,47 @@ public sealed class Continent:SmartEnum<Continent,string>
 	public static Continent Africa = new ("Africa","AF");
 	public static Continent SouthAmerica = new ("South America","SA");
 	public static Continent Asia = new ("Asia","AS");
+}
+
+public class ContinentConverter : JsonConverter<Continent>
+{
+	public override void WriteJson(
+		JsonWriter writer,
+		Continent? value,
+		JsonSerializer serializer)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override Continent? ReadJson(
+		JsonReader reader,
+		Type objectType,
+		Continent? existingValue,
+		bool hasExistingValue,
+		JsonSerializer serializer)
+	{
+		do
+		{
+			reader.Read();
+		} while ((string)reader.Value != "Value");
+		reader.Read();
+		string s = (string)reader.Value;
+		var toReturn = 
+		s switch
+		{
+			"OC" => Continent.Oceania,
+			"NA" => Continent.NorthAmerica,
+			"EU" => Continent.Europe,
+			"AF" => Continent.Africa,
+			"SA" => Continent.SouthAmerica,
+			"AS" => Continent.Asia,
+			_ => null,
+		};
+		while (reader.TokenType != JsonToken.EndObject) 
+		{
+			reader.Read();
+		}
+
+		return toReturn;
+	}
 }
