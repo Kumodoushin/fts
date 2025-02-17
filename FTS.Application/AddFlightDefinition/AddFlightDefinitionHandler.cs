3d91bf6f39342e3e74149158052acc888f4e0351
Kumodoushin <Kumodoushin@users.noreply.github.com>
2025-02-17 07:35:04 +0100

    add flight definition handler

---
+++ FTS.Application/AddFlightDefinition/AddFlightDefinitionHandler.cs
@@ -0,0 +1,29 @@
+﻿using FTS.Domain;
+using OneOf;
+namespace FTS.Application.AddFlightDefinition;
+
+public class AddFlightDefinitionHandler
+{
+	private readonly Dictionary<FlightId,Flight> _data;
+	public AddFlightDefinitionHandler(Dictionary<FlightId,Flight> data)
+	{
+		_data = data;
+	}
+	
+	public OneOf<bool,FlightDefinitionAlreadyExists> Handle(Flight flightToAdd)
+	{
+		if (_data.ContainsKey(flightToAdd.Id))
+		{
+			return new FlightDefinitionAlreadyExists();
+		}
+
+		_data.Add(flightToAdd.Id, flightToAdd);
+
+		return true;
+	}
+
+	public class FlightDefinitionAlreadyExists : Exception
+	{
+		
+	}
+}
