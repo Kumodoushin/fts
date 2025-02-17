c7487277a69db8709d16bc83f21d48595ac1af0e
Kumodoushin <Kumodoushin@users.noreply.github.com>
2025-02-17 01:06:35 +0100

    basic project setup and helper apps

---
+++ AirportDatabaseCreation/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FTS.Domain;
+using Newtonsoft.Json;
+
+var dir = new DirectoryInfo("icaoData");
+if (!dir.Exists)
+{
+	Console.WriteLine("No data to process - missing icaoData directory");
+}
+
+var files = dir.EnumerateFiles("*.json");
+Console.WriteLine($"Found {files.Count()} airports");
+var airports = new List<Airport>();
+foreach (var file in files)
+{
+	var airportData = JsonConvert.DeserializeObject<AirportJson>(file.OpenText().ReadToEnd());
+	airports.Add(new()
+	{
+		IATA = airportData.Iata_Code, 
+		ICAO = airportData.Ident,
+		Name = airportData.Name,
+		Country = airportData.Country.Name,
+		CountryISO = airportData.Iso_Country,
+		Continent = Continent.FromValue(airportData.Continent),
+		Latitude = airportData.Latitude_deg,
+		Longitude = airportData.Longitude_deg,
+	});
+}
+Console.WriteLine($"Processed {airports.Count} airports");
+
+//OC, NA, EU, AF, SA, AS
+Console.WriteLine("done");
+Console.ReadLine();
+
+public class AirportJson
+{
+	public string Ident { get; set; }
+	public string Name { get; set; }
+	public string Continent { get; set; }
+	public string Iata_Code { get; set; }
+	public string Iso_Country { get; set; }
+	public CountryT Country { get; set; }
+	
+	public double Latitude_deg { get; set; }
+	public double Longitude_deg { get; set; }
+
+	public class CountryT
+	{
+		public string Name { get; set; }
+	}
+}
---
+++ DataHarvestingHelper/Program.cs
@@ -0,0 +1,52 @@
+﻿var client = new HttpClient()
+{
+	BaseAddress = new Uri("https://airportdb.io/api/v1/airport/"),
+};
+
+string apiToken = string.Empty; //get yours from airportdb.io
+
+if (string.IsNullOrWhiteSpace(apiToken))
+{
+	return;
+}
+
+var dir = new DirectoryInfo("icaoData");
+if (!dir.Exists)
+{
+	dir.Create();
+}
+
+using var textReader = new StreamReader("ICAO.txt");
+
+while (!textReader.EndOfStream)
+{
+	var ICAO = (await textReader.ReadLineAsync() ?? string.Empty).Trim();
+
+	if (ICAO.StartsWith('#') || dir.GetFiles($"{ICAO}.*").Any())
+	{
+		Console.WriteLine($"Skipping {ICAO}");
+		continue;
+	}
+	
+	Console.Write($"Downloading {ICAO}...");
+	var response = await client.GetAsync($"{ICAO}?apiToken={apiToken}");
+	
+	if (response.IsSuccessStatusCode)
+	{
+		var data = await response.Content.ReadAsStringAsync();
+		
+		Console.Write($"\tSaving {ICAO}.json...");
+		File.WriteAllText(
+			Path.Combine("icaoData", $"{ICAO}.json"),
+			data);
+	}
+	else
+	{
+		Console.Write($"\tFailed, details saved to {ICAO}.error...");
+		
+		File.WriteAllText(
+			Path.Combine("icaoData", $"{ICAO}.error"),
+			 response.ToString());
+	}
+	Console.WriteLine($"\tDone");
+}
