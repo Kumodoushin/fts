1b5f1e38ff30f1c2d881c82ec1852bc11d1d55d0
Kumodoushin <Kumodoushin@users.noreply.github.com>
2025-02-17 01:38:49 +0100

    updates to Flight (equality checks)

--- FTS.Domain/FlightDefinition/Flight.cs
+++ FTS.Domain/FlightDefinition/Flight.cs
@@ -22,4 +22,12 @@ public class Flight
 	private List<DayOfWeek> _departureDays;
 
 	public override string ToString() => $"{Id} from {_departureAirport} to {_destinationAirport} leaving at {_departure.ToShortTimeString()} local time on {string.Join(", ",_departureDays.Select(x=>x.ToString()))}";
+
+	public override int GetHashCode() => Id.GetHashCode();
+
+	public override bool Equals(object? obj) => 
+		obj is not null && obj is Flight otherFlight && this == otherFlight;
+
+	public static bool operator ==(Flight a, Flight b) => a.Id == b.Id;
+	public static bool operator !=(Flight a, Flight b) => !(a == b);
 }
