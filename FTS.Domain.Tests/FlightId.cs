2e4eef5f8021a86581d060c7f15573de7c019831
Kumodoushin <Kumodoushin@users.noreply.github.com>
2025-02-17 01:11:51 +0100

    FlightId definition and tests

---
+++ FTS.Domain.Tests/FlightId.cs
@@ -0,0 +1,134 @@
+using Bogus;
+using FsCheck;
+using FsCheck.Fluent;
+
+namespace FTS.Domain.Tests;
+
+public class FlightIdTests
+{
+	
+	[Theory]
+	[ClassData(typeof(ValidInputStringsForFlightId))]
+	public void ProperInputStringCreatesValidInstance(string inputString)
+	{
+		var flightId = new FlightId(inputString);
+	}
+
+	[Theory]
+	[ClassData(typeof(InvalidInputStringsForFlightIdAndExpectedExceptions))]
+	public void InvalidInputStringThrowsSpecificExceptions(Type expectedException, string inputString)
+	{
+		Assert.Throws(
+			expectedException,
+			testCode: () =>
+				{
+					var id = new FlightId(inputString);
+				});
+	}
+
+	public static Gen<string> _iataGen = Gen.Elements((new List<string> { "ASD", "BSD", "CSD" }).AsEnumerable());
+	public static Gen<string> _lastBitGen = Gen.Elements((new List<string> { "ASD", "BSD", "CSD" }).AsEnumerable());
+	
+	[Fact]
+	public void IntancesCreatedFromSameDataAreEquivalent()
+	{
+		Prop.ForAll(
+			_iataGen.ToArbitrary(),
+			Gen.Choose(1, 99_999).ToArbitrary(),
+			_lastBitGen.ToArbitrary(),
+			(
+				iata,
+				num,
+				lastSeg) =>
+			{
+				var inst1 = new FlightId($"{iata} {num:D5} {lastSeg}");
+				var inst2 = new FlightId($"{iata} {num} {lastSeg}");
+
+				return inst1 == inst2;
+			})
+			.VerboseCheckThrowOnFailure();
+	}
+
+	public class InvalidInputStringsForFlightIdAndExpectedExceptions : TheoryData<Type,string>
+	{
+		public InvalidInputStringsForFlightIdAndExpectedExceptions()
+		{
+			Add(typeof(FlightId.InvalidInputIdFormat), "1");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLM1");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLM1BCA");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLM 1");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLM 12");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLM 123");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLM 1234");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLM 12345");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLMA 123456");
+			Add(typeof(FlightId.InvalidInputIdFormat), "1 BCA");
+			Add(typeof(FlightId.InvalidInputIdFormat), "12 BCA");
+			Add(typeof(FlightId.InvalidInputIdFormat), "123 BCA");
+			Add(typeof(FlightId.InvalidInputIdFormat), "1234 BCA");
+			Add(typeof(FlightId.InvalidInputIdFormat), "12345 BCA");
+			Add(typeof(FlightId.InvalidInputIdFormat), "123456 BCAA");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLMA BCAA");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLA BAA");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KA BA");
+			Add(typeof(FlightId.InvalidInputIdFormat), "K B");
+			Add(typeof(FlightId.InvalidInputIdFormat), "KLM 123456 BCAA extrabits");
+
+			Add(typeof(FlightId.InvalidIATAAirlineCodeFormat), $"KLMA 00001 BCA");
+			Add(typeof(FlightId.InvalidIATAAirlineCodeFormat), $"KLMBA 00001 BCA");
+			Add(typeof(FlightId.InvalidIATAAirlineCodeFormat), $"KA 00001 BCA");
+			Add(typeof(FlightId.InvalidIATAAirlineCodeFormat), $"A 00001 BCA");
+			
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KLm 00001 BCA");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KlM 00001 BCA");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"kLM 00001 BCA");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"1LM 00001 BCA");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"K2M 00001 BCA");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KL3 00001 BCA");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KLą 00001 BCA");
+			
+			Add(typeof(FlightId.InvalidFlightNumberValue), $"KLM 00000 BCA");
+			Add(typeof(FlightId.InvalidFlightNumberValue), $"KLM 100001 BCA");
+			var f = new Faker();
+			var rnd = f.Random;
+			
+			for (int i = 0; i < 100; i++)
+			{
+				Add(typeof(FlightId.InvalidFlightNumberValue), $"KLM {rnd.Int(100_000, Int32.MaxValue)} BCA");
+			}
+			for (int i = 0; i < 100; i++)
+			{
+				Add(typeof(FlightId.InvalidFlightNumberValue), $"KLM {rnd.Int(Int32.MinValue, 0)} BCA");
+			}
+			
+			Add(typeof(FlightId.InvalidFinalSegmentLength), $"KLM 00001 BCAC");
+			Add(typeof(FlightId.InvalidFinalSegmentLength), $"KLM 00001 BCABC");
+			Add(typeof(FlightId.InvalidFinalSegmentLength), $"KLM 00001 BC");
+			Add(typeof(FlightId.InvalidFinalSegmentLength), $"KLM 00001 B");
+			
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KLM 00001 bCA");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KLM 00001 BcA");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KLM 00001 BCa");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KLM 00001 1CA");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KLM 00001 B2A");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KLM 00001 BC3");
+			Add(typeof(FlightId.InvalidSegmentFormat), $"KLM 00001 BCą");
+			
+		}
+	}
+
+	public class ValidInputStringsForFlightId : TheoryData<string>
+	{
+		private static string _characterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		public ValidInputStringsForFlightId()
+		{
+			var f = new Faker();
+			var rnd = f.Random;
+			
+			for (int i = 0; i < 10000; i++)
+			{
+				Add($"{rnd.String2(3, _characterSet)} {rnd.Int(1, 99999)} {rnd.String2(3, _characterSet)}");
+			}
+		}
+	}
+}
---
+++ FTS.Domain/FlightDefinition/FlightId.cs
@@ -0,0 +1,104 @@
+﻿namespace FTS.Domain;
+
+public struct FlightId
+{
+	private readonly string _IATAAirlineSegment;
+	private readonly int _number;
+	private readonly string _postfix;
+	private static bool AnyOfCharsIsNotAsciiUpperLetter(string value) =>
+		!char.IsAsciiLetterUpper(value[0]) 
+		|| !char.IsAsciiLetterUpper(value[1]) 
+		|| !char.IsAsciiLetterUpper(value[2]);
+
+	public FlightId(string id)
+	{
+		var segments = id.Split(" ");
+		if (segments.Length != 3)
+		{
+			throw new InvalidInputIdFormat();
+		}
+
+		if (segments[0].Length != 3)
+		{
+			throw new InvalidIATAAirlineCodeFormat();
+		}
+		if (AnyOfCharsIsNotAsciiUpperLetter(segments[0]))
+		{
+			throw new InvalidSegmentFormat(segments[0]);
+		}
+		_IATAAirlineSegment = segments[0];
+		
+		_number = int.Parse(segments[1]);
+		if (_number is > 99999 or < 1)
+		{
+			throw new InvalidFlightNumberValue();
+		}
+		
+		if (segments[2].Length != 3)
+		{
+			throw new InvalidFinalSegmentLength();
+		}
+
+		if (AnyOfCharsIsNotAsciiUpperLetter(segments[2]))
+		{
+			throw new InvalidSegmentFormat(segments[2]);
+		}
+		_postfix = segments[2];
+	}
+
+	public override string ToString() => $"{_IATAAirlineSegment} {_number:D5} {_postfix}";
+
+	public override int GetHashCode() => HashCode.Combine(_IATAAirlineSegment,_number,_postfix);
+
+	public override bool Equals(object? obj) => 
+		obj is not null 
+		&& obj is FlightId otherFlight 
+		&& this == otherFlight;
+
+	public static bool operator ==(FlightId a, FlightId b) =>
+		a._IATAAirlineSegment == b._IATAAirlineSegment 
+		&& a._number == b._number 
+		&& a._postfix == b._postfix;
+
+	public static bool operator !=(FlightId a, FlightId b) => !(a == b);
+
+	public class InvalidFlightNumberValue : Exception
+	{
+		public InvalidFlightNumberValue() :
+			base ("Second part of FlightId, the Flight number must be greater than 0 and less than 100'000")
+		{
+		}
+	}
+
+	public class InvalidSegmentFormat : Exception
+	{
+		public InvalidSegmentFormat(string value) : 
+			base($"Provided value \"{value}\" does not match the 3 letter format")
+		{
+		}
+	}
+
+	public class InvalidFinalSegmentLength : Exception
+	{
+		public InvalidFinalSegmentLength() : 
+			base("Last segment of FlightId, must have 3 letters")
+		{
+		}
+	}
+
+	public class InvalidIATAAirlineCodeFormat : Exception
+	{
+		public InvalidIATAAirlineCodeFormat() : 
+			base("First part of FlightId, the IATA Airline Code must have 3 letters")
+		{
+		}
+	}
+	
+	public class InvalidInputIdFormat : Exception
+	{
+		public InvalidInputIdFormat() : 
+			base("Provided FlightId string has invalid format, expected \"AAA 11111 AAA\"")
+		{
+		}
+	}
+}
